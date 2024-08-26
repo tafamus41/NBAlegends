03c355c287c07c515e09794205b0c38b36a77420
asimov <seyranmustafa4@gmail.com>
2024-08-27 02:36:40 +0300

    nbalegends

---
+++ nbalegends/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/nba-logo.png"
+
+const Header = () => {
+  return (
+    <div>
+      <div className="logoandtext">
+        <img src={logo} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
