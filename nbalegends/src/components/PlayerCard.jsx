ad01a9aae53f1636ba8605feded37023eea305a8
asimov <seyranmustafa4@gmail.com>
2024-08-27 18:30:53 +0300

    update

--- nbalegends/src/components/PlayerCard.jsx
+++ nbalegends/src/components/PlayerCard.jsx
@@ -1,11 +1,23 @@
-const PlayerCard = ({ name, img, statistics }) => {
-console.log(statistics);
-  const change=()=>{}
+import React, { useState } from "react";
+
+const PlayerCard = ({ img, name, statistics }) => {
+  const [showStats, setShowStats] = useState(false);
+  const change = () => setShowStats(!showStats);
   return (
-        <div className="card col-md-6 col-lg-4 col-xl-3 ">
-            <img src={img} alt="" />
-            <p className="text-center fw-bold h4 p-3">{name}</p>
-        </div>
+    <div onClick={change} className="card col-md-6 col-lg-4 col-xl-3 ">
+      {showStats ? (
+        <ul className="listStats">
+          {statistics.map((stat, index) => (
+            <li key={index}>{stat}</li>
+          ))}
+        </ul>
+      ) : (
+        <>
+          <img src={img} alt={`${name}`} />
+          <p className="text-center fw-bold h4 p-3">{name}</p>
+        </>
+      )}
+    </div>
   );
 };
 
